671819af95e423f434e27d0d112f6f33afdff3da
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-10-02 17:07:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var car,wall;
+var speed,weight;
+function setup() {
+  createCanvas(1400,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+ car= createSprite(50, 200, 50, 50);
+ wall= createSprite(1300,200,50,200);
+ car.shapeColor="blue";
+ wall.shapeColor="hotpink";
+ car.velocityX= speed;
+ 
+}
+
+function draw() {
+  background(255,255,255); 
+  if (wall.x-car.x<car.width/2+wall.width/2    ){
+    car.velocityX=0;
+    var deform= 0.5*weight*speed*speed/22500;
+    if(deform>=180){
+      car.shapeColor="red";
+
+    } 
+    if(deform<180 && deform >100){
+      car.shapeColor="yellow";
+      
+    }
+    if(deform<=100){
+      car.shapeColor="green";
+      
+    }
+   
+    }
+
+  drawSprites();
+
+}
